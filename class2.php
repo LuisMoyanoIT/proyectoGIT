e028707ef15807326b77fa21835084e53152859a
LuisMoyanoIT <luismocru@gmail.com>
2021-10-07 08:53:19 -0500

    cambio en la rama main para ver en Luis

--- class2.php
+++ class2.php
@@ -43,7 +43,7 @@ include("modelos/empleadosBackend.php");
     <div class="container">
         <div class="content">
             <!--A TRAVES DE LA PROPIEDAD TEXT-ALIGN SE PUEDE DARLE UNA POSICION A CONVENIENCIA A NUESTRO TEXTO-->
-            <h1 style="text-align: center;">Rama de Luis Moyano</h1>
+            <h1 style="text-align: center;">Cambios en la rama main que quiero ver en rama LUi</h1>
             <hr>
             <div class="row">
                 <div class="col-4">
